08ef8bd01b0259023d71c3203e9fbc863991d06e
Jacob Overschmidt <jacoboverschmidt@jacobs-macbook-pro.local>
2017-07-14 23:49:44 -0500

    lastcommit

--- Colors/Colors/ColorsViewController.swift
+++ Colors/Colors/ColorsViewController.swift
@@ -11,19 +11,17 @@ import UIKit
 class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var colorsTableView: UITableView!
-    
+
     var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-   colorsTableView.dataSource = self
+        colorsTableView.dataSource = self
         colorsTableView.delegate = self
         
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -36,7 +34,7 @@ class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colors.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
         
@@ -47,13 +45,13 @@ class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return cell
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
